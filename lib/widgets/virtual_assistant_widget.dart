d5dc0e2c3cd6ccc460f1220731ef1f4ec3c21160
Jose Fernandez <josefernandez012571@gmail.com>
2026-07-21 01:04:48 -0500

    Apply user's flawless pixel matrix validation

--- lib/widgets/virtual_assistant_widget.dart
+++ lib/widgets/virtual_assistant_widget.dart
@@ -177,17 +177,17 @@ class _PixelArtPainter extends CustomPainter {
   _PixelArtPainter({required this.color});
 
   static const List<String> sprite = [
-    "  ##########  ", // 0 (Head top)
-    "  ##########  ", // 1
-    "  ##########  ", // 2
-    "  ## #### ##  ", // 3 (Eye top)
-    "  ## #### ##  ", // 4 (Eye mid)
-    "#### #### ####", // 5 (Eye bottom + Arm top)
-    "##############", // 6 (Body + Arm mid)
-    "##############", // 7 (Body + Arm bottom)
-    "  # #    # #  ", // 8 (Legs)
-    "  # #    # #  ", // 9 (Legs)
-    "  # #    # #  ", // 10 (Legs)
+    "  ##########  ", // 3 filas para la frente alta
+    "  ##########  ", 
+    "  ##########  ", 
+    "  ## #### ##  ", // 3 filas para los ojos estirados
+    "  ## #### ##  ", 
+    "#### #### ####", // Aquí arrancan los brazos
+    "##############", // 2 filas sólidas debajo de los ojos
+    "  ##########  ", // 1 fila sólida de cuerpo antes de piernas
+    "   # #  # #   ", // 3 filas para las piernas largas
+    "   # #  # #   ", 
+    "   # #  # #   ", 
   ];
 
   @override
